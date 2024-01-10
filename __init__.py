16c3d040944805d9d4f879ca7f6738f9e6eaf450
H_ALZ <hessam.la@gmail.com>
2024-01-09 19:39:39 -0500

    first commit

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from .code.recursive_namespace import RecursiveNamespace
+
+__all__ = ['RecursiveNamespace']
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RecursiveNamespace', 
+    version='0.1.0',  
+    author='Hessam LA',
+    author_email='hlotfali@purdue.edu',  
+    description='Recursive Namespace. An extension of SimpleNamespace',  # A short description
+    long_description=open('README.md').read(),  # Long description read from the readme
+    long_description_content_type='text/markdown',  # Type of the long description
+    url='https://github.com/HessamLa/RecursiveNamespace',  # Link to your package's GitHub repo or website
+    packages=find_packages(),  # Find all packages and subpackages
+    install_requires=[  # List of dependencies
+    ],
+    classifiers=[  # Classifiers help users find your project
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Minimum version requirement of the package
+)

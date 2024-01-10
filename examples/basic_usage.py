f1a370a35bde48fd0c44d71a0aa98772f728439c
HessamLa <hessam.la@gmail.com>
2024-01-09 22:27:54 -0500

    new examples added

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from RecursiveNamespace import RecursiveNamespace
 
 # Creating a simple recursive namespace
@@ -10,7 +11,32 @@ data = {
     }
 }
 
-rn = RecursiveNamespace(data)
+print("------------")
+print("data dictionary:")
+pprint(data)
 
-print(rn.name)  # Output: John Doe
-print(rn.address.street)  # Output: 123 Main St
+rn = RecursiveNamespace(data)
+print("------------")
+print("rn:")
+print(rn)
+
+print("------------")
+print("rn.name:", rn.name) # Output: John Doe
+print("rn.address.street:", rn.address.street) # Output: 123 Main St
+
+print("------------")
+print("to_dict():")
+print(rn.to_dict())
+
+print("------------")
+print("to_dict() flattening with '_' separator:")
+print(rn)
+pprint(rn.to_dict(flatten_sep='_'))
+
+
+print("------------")
+rn.scores = RecursiveNamespace({'score-1': 98, 'score-2': 100})
+print(f"rn.scores.score_1: {rn.scores.score_1}")
+print(f"rn.scores.score_2: {rn.scores.score_2}")
+rn.scores.score_3 = 99
+print(f"rn.scores.score_3: {rn.scores.score_3}")

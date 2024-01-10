13833bb125455dc5cb2e075370fd896600d86de4
HessamLa <hessam.la@gmail.com>
2024-01-09 23:27:52 -0500

    added my preferred usage

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,7 +1,35 @@
 from pprint import pprint
-from RecursiveNamespace import RecursiveNamespace
+from RecursiveNamespace import RecursiveNamespace as RN
 
-# Creating a simple recursive namespace
+# I prefer to use as the following
+results = RN(
+    params=RN(
+        alpha=1.0,
+        beta=2.0,
+        dataset_name='dataset_name',
+        dataset_path='dataset_path',
+        classifier_name='classifier_name',
+    ),
+    metrics=RN(
+        accuracy=0.0,
+        f1=0.0,
+    )
+)
+
+print(results.params.k1)
+print(results.metrics.accuracy)
+# then I can add more information on the fly
+results.experiment_name = 'experiment_name'
+results.params.dataaset_version = 'dataset_version'
+results.params.gamma = 3.0
+
+# Then I'd convert it to dictionary
+output_dict = results.to_dict()
+
+# or just convert metrics to dictionary
+metrics_dict = results.metrics.to_dict()
+
+# You can also pass a full dictionary to RecursiveNamespace
 data = {
     'name': 'John Doe',
     'age': 30,
@@ -15,7 +43,7 @@ print("------------")
 print("data dictionary:")
 pprint(data)
 
-rn = RecursiveNamespace(data)
+rn = RN(data)
 print("------------")
 print("rn:")
 print(rn)
@@ -34,8 +62,10 @@ pprint(rn.to_dict(flatten_sep='_'))
 
 
 print("------------")
-rn.scores = RecursiveNamespace({'score-1': 98.4, 'score-2': 100})
+rn.scores = RN({'score-1': 98.4, 'score-2': 100})
 print(f"rn.scores.score_1: {rn.scores.score_1}")
 print(f"rn.scores.score_2: {rn.scores.score_2}")
 rn.scores.score_3 = 99.07
 print(f"rn.scores.score_3: {rn.scores.score_3}")
+
+# I personally would like to use it like the following

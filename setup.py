ec74722cab6488d999318ca36750369ffed7ca54
HessamLa <hessam.la@gmail.com>
2024-05-07 17:03:12 -0400

    updated setup script

--- setup.py
+++ setup.py
@@ -1,29 +1,24 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='RecursiveNamespace', 
     version='0.2.0',  
-    author='Hessam LA',
-    author_email='hlotfali@purdue.edu',  
-    description='Recursive Namespace. An extension of SimpleNamespace',  # A short description
-    long_description=open('README.md').read(),  # Long description read from the readme
-    long_description_content_type='text/markdown',  # Type of the long description
-    url='https://github.com/HessamLa/RecursiveNamespace',  # Link to your package's GitHub repo or website
- 
-    # package_dir={'RecursiveNamespace': 'src/RecursiveNamespace'},
-    # packages=['RecursiveNamespace']
-
+    author='Hamidreza Lotfalizadeh (Hessam)',
+    author_email='hlotfali_at_purdue_edu',  
+    description='Recursive Namespace. An extension of SimpleNamespace',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/HessamLa/RecursiveNamespace', 
     package_dir={'': 'src'},
-    # package_dir={'RecursiveNamespace': 'src/RecursiveNamespace'},
     packages=find_packages(where='src'),
-    # packages=find_packages(where='src'),
-    # packages=find_packages(where='.', include=['RecursiveNamespace']),  # Find all packages and subpackages
-    install_requires=[  # List of dependencies
-    ],
-    classifiers=[  # Classifiers help users find your project
+    install_requires=[],
+    classifiers=[  
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',  
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',  # Minimum version requirement of the package
+    python_requires='>=3.6',
 )
